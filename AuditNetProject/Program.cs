5d672983407fa417ef3b51b0317a1cf8bf89baa4
Fred Maclean <macleash90@gmail.com>
2023-10-13 09:50:46 +0000

    Added swagger, Audit.net packages

--- AuditNetProject/Program.cs
+++ AuditNetProject/Program.cs
@@ -1,7 +1,13 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Audit.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddControllers();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -12,6 +18,27 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+if (app.Environment.IsDevelopment())
+{
+    //StrconfigFile = "appsettings.Development.json";
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+var configuration = builder.Configuration;
+
+Audit.Core.Configuration.Setup()
+    .UseSqlServer(config => config
+    .ConnectionString((string)configuration.GetConnectionString("DBConn"))
+    .Schema("dbo")
+    .TableName("Events")
+    .IdColumnName("EventId")
+    .JsonColumnName("JsonData")
+    .LastUpdatedColumnName("LastUpdatedDate")
+    //.CustomColumn("MerchantId", ev => 4)
+    .CustomColumn("EventType", ev => ev.EventType)
+    .CustomColumn("UserId", ev => ev.Environment.UserName));
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
